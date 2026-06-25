a5756c6cd66d18e772827e15eea62906777c9bed
Keven de Souza <kevensouza8000@gmail.com>
2026-06-25 16:57:27 -0300

    Educação & Tecnologia Responsividade

--- components/sections/EducationTechSection.tsx
+++ components/sections/EducationTechSection.tsx
@@ -11,36 +11,36 @@ import {educationData, techCategories} from "@/data/educationdata"
 
 export function EducationTechSection() {
   return (
-    <section id="sobre" className="py-20 bg-background transition-colors duration-300">
+    <section id="sobre" className="py-10 sm:py-20 bg-background transition-colors duration-300">
       <div className="container-lp">
         
         {/* Cabeçalho da Seção */}
-        <div className="mb-16 xl:max-w-1/2 ">
+        <div className=" mb-10 sm:mb-16 xl:max-w-1/2 ">
           <h2 className="text-3xl max-[480px]:text-2xl sm:text-4xl text-foreground font-bold mb-4">
             Educação & Tecnologias
           </h2>
           <p className="text-surface-text leading-relaxed">
-            Minha base acadêmica e o ecossistema de ferramentas que utilizo para <span className="bg-degrade bg-clip-text text-transparent !font-geist-sans">construir e escalar soluções.</span>
+            Minha base acadêmica e o ecossistema de ferramentas que utilizo para <span className="bg-degrade bg-clip-text text-transparent">construir e escalar soluções.</span>
           </p>
         </div>
 
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
           
           {/* COLUNA ESQUERDA: Educação e Currículo */}
-          <div className="lg:col-span-5 flex flex-col gap-8">
+          <div className="lg:col-span-5 flex flex-col gap-4 sm:gap-8">
             <motion.div 
               initial={{ opacity: 0, y: 20 }}
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ once: true, amount: 0.3 }}
               transition={{ duration: 0.5 }}
-              className="bg-surface/10 border border-surface/20 rounded-3xl p-8"
+              className="bg-surface/10 border border-surface/20 rounded-3xl py-8 px-4 sm:px-8"
             >
               <div className="flex items-center gap-3 mb-8">
                 <GraduationCap className="size-8 text-primary" />
                 <h3 className="text-2xl text-foreground font-bold">Formação</h3>
               </div>
 
-              <div className="flex flex-col gap-6">
+              <div className="flex flex-col gap-8">
                 {educationData.map((item, index) => (
                   <div key={index} className="flex items-start gap-4">
                     <span className="mt-1 flex shrink-0 items-center justify-center size-8 rounded-full bg-surface/20 text-text-color">
@@ -70,7 +70,7 @@ export function EducationTechSection() {
                 href="/curriculo.pdf" // Substitua pelo caminho real do seu PDF
                 target="_blank"
                 rel="noopener noreferrer"
-                className="group flex items-center justify-center gap-3 w-full bg-primary hover:bg-primary-hover text-background font-bold py-4 px-8 rounded-2xl transition-colors duration-300 shadow-lg cursor-pointer"
+                className="group hidden lg:flex items-center justify-center gap-3 w-full bg-primary hover:bg-primary-hover text-background font-bold py-4 px-8 rounded-2xl transition-colors duration-300 shadow-lg cursor-pointer"
               >
                 <span>Baixar Currículo (PDF)</span>
                 <Download className="size-5 transition-transform group-hover:-translate-y-1" />
@@ -90,7 +90,7 @@ export function EducationTechSection() {
                   whileInView={{ opacity: 1, x: 0 }}
                   viewport={{ once: true, amount: 0.3 }}
                   transition={{ duration: 0.5, delay: index * 0.1 }}
-                  className="bg-surface/5 border border-surface/15 transition-colors duration-200 hover:border-primary/80 rounded-3xl p-6 sm:p-8"
+                  className="bg-surface/5 border border-surface/15 transition-colors duration-200 hover:border-primary/80 rounded-3xl py-8 px-4 sm:px-8"
                 >
                   <div className="flex flex-wrap items-center justify-between gap-4 mb-6">
                     <div className="flex items-center gap-3">
@@ -147,11 +147,30 @@ export function EducationTechSection() {
                       )
                     })}
                   </div>
+                  
                 </motion.div>
+                
               )
             })}
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true, amount: 0.3 }}
+              transition={{ duration: 0.5, delay: 0.2 }}
+              className="flex justify-center"
+            >
+              <a 
+                href="/curriculo.pdf" // Substitua pelo caminho real do seu PDF
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group flex lg:hidden items-center justify-center gap-3 bg-primary hover:bg-primary-hover text-background font-bold py-4 px-8 sm:px-14 rounded-2xl transition-colors duration-300 shadow-lg "
+              >
+                <span>Baixar Currículo (PDF)</span>
+                <Download className="size-5 transition-transform group-hover:-translate-y-1" />
+              </a>
+            </motion.div>            
           </div>
-
+            
         </div>
       </div>
     </section>
